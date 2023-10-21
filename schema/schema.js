bfb24ee2cc4485267058706dfa42a1a9f378fa0b
kushalshrestha <kushalshr@gmail.com>
2023-10-21 09:39:40 -0500

    feat: Nested Queries, Multiple RootQuery and Bidirectional Relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt , GraphQLSchema} = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql;
 // const _ = require("lodash");  /* Replaced with axios*/
-const axios = require('axios');
+const axios = require("axios");
 
 /* hardcoded */
 // const users = [
@@ -9,13 +15,41 @@ const axios = require('axios');
 //   { id: "47", firstName: "Kushal", age: 21 },
 // ];
 
+const API_BASE_URL = `http://localhost:3000`;
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: () => ({
+    /* Creating arrow function to resolve circular references*/
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        console.log(API_BASE_URL + `/companies/${parentValue.id}/users`)
+        return axios
+          .get(API_BASE_URL + `/companies/${parentValue.id}/users`)
+          .then((res) => res.data);
+      },
+    },
+  }),
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(API_BASE_URL + `/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -27,13 +61,23 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         /* a function who does the searches in the db and provide you the response*/
         // return _.find(users, { id: args.id }); /* commented after not using lodash for hardcoded response*/
-        return axios.get(`http://localhost:3000/users/${args.id}`)
-        .then(resp => resp.data);
+        return axios
+          .get(API_BASE_URL + `/users/${args.id}`)
+          .then((resp) => resp.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(API_BASE_URL + `/companies/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
